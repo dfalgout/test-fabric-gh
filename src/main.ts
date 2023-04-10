bdff63a8a762c729a95c99b8e89f9ddc63b71aca
lambda <dustinafalgout@gmail.com>
2023-04-10 15:51:21 -0700

    abstract out project data

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import * as core from '@actions/core'
 
 async function run(): Promise<void> {
   try {
-    core.debug('test')
+    core.setOutput('project_id', '728919945741')
+    core.setOutput('project_name', 'fabric')
+    core.setOutput('service_account', 'dev-104@fabric-375222.iam.gserviceaccount.com')
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
